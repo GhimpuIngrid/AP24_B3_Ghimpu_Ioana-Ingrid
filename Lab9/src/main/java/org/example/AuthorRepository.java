d3371e4f55eb401a562470803269b4a95b6fc7f6
GhimpuIngrid <ghimpuingrid@gmail.com>
2024-05-10 22:57:09 +0300

    modificare mica in DB.java

--- Lab9/src/main/java/org/example/AuthorRepository.java
+++ Lab9/src/main/java/org/example/AuthorRepository.java
@@ -1,5 +1,8 @@
 package org.example;
 
+
+import org.example.entities.Author;
+
 public class AuthorRepository {
 
 }
--- Lab9/src/main/java/org/example/DB.java
+++ Lab9/src/main/java/org/example/DB.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DB {
     private static EntityManagerFactory factory;
 
